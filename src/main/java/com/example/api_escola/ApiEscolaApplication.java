a8d365d1a1f2c671f6b39c5de28661ba4ed3225d
Leon Dale <81538872+LeonDale2001@users.noreply.github.com>
2025-09-17 20:16:59 -0300

    ♻️ refactor(config): usa variáveis de ambiente e H2 para testes do maeven

--- src/main/java/com/example/api_escola/ApiEscolaApplication.java
+++ src/main/java/com/example/api_escola/ApiEscolaApplication.java
@@ -2,12 +2,23 @@ package com.example.api_escola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ApiEscolaApplication {
+    public static void main(String[] args) {
+        // Carrega automaticamente o .env
+        Dotenv dotenv = Dotenv.configure()
+                               .directory("./") // raiz do projeto
+                               .ignoreIfMalformed()
+                               .ignoreIfMissing()
+                               .load();
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiEscolaApplication.class, args);
-	}
+        // Define variáveis do sistema para o Spring Boot ler
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
+        SpringApplication.run(ApiEscolaApplication.class, args);
+    }
 }
--- src/test/java/com/example/api_escola/ApiEscolaApplicationTests.java
+++ src/test/java/com/example/api_escola/ApiEscolaApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.api_escola;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiEscolaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
