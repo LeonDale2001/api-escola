ba506e068fdf3fe92eb7943f5613d8e7e2819e79
Leon Dale <81538872+LeonDale2001@users.noreply.github.com>
2025-09-24 20:16:21 -0300

    ♻️Refactor: espaçamento de metodos para melhor visualização

--- src/main/java/com/example/api_escola/dto/ProfessorDTO.java
+++ src/main/java/com/example/api_escola/dto/ProfessorDTO.java
@@ -16,8 +16,10 @@ public class ProfessorDTO {
     // Getters e setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+    
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
--- src/main/java/com/example/api_escola/model/Inscricao.java
+++ src/main/java/com/example/api_escola/model/Inscricao.java
@@ -25,10 +25,13 @@ public class Inscricao {
     // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public LocalDateTime getDataHora() { return dataHora; }
     public void setDataHora(LocalDateTime dataHora) { this.dataHora = dataHora; }
+
     public Aluno getAluno() { return aluno; }
     public void setAluno(Aluno aluno) { this.aluno = aluno; }
+    
     public Turma getTurma() { return turma; }
     public void setTurma(Turma turma) { this.turma = turma; }
 }
